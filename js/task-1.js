24336a6f04a9b8452b5fe2dcd510c0ba5adbf0f3
Yura111 <yur8594@gmail.com>
2020-07-17 13:14:53 +0300

    start

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+ const logitems = function(array) {
+     let index = 1;
+     for (const item of array) {
+         console.log(`${index} - ${item}`);
+         index += 1;
+     }
+ }
+ logitems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+ logitems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+ 
